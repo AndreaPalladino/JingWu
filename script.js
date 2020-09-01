cbdc931999c9cfc74aec7fd98278854907cae42b
Andrea Palladino <andreagpalladino@gmail.com>
2020-09-01 12:26:55 +0200

    sito completato, test da mobile, tutto ok

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.querySelector('#wrapper').addEventListener('click', ()=>{
     let where = document.querySelector('#where')
     let kids = document.querySelector('#kids')
     let gallery = document.querySelector('#gallery')
-    let contact = document.querySelector('#contact')
+    
     let overlay = document.querySelector('.overlay')
 
 
@@ -15,14 +15,9 @@ document.querySelector('#wrapper').addEventListener('click', ()=>{
     where.classList.toggle('active')
     kids.classList.toggle('active')
     gallery.classList.toggle('active')
-    contact.classList.toggle('active')
+   
     overlay.classList.toggle('active')
 
 
 })
 
-document.addEventListener('scroll',()=>{
-    let bg = document.querySelector('.wushu')
-
-    bg.style.backgroundSize = `100%,${window.pageYOffset / 10}%`
-})
